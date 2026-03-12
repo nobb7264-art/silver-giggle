e7642a2611f8b00a7d24c06f6d3c150443c01462
nobb7264-art <nobb7264@gmail.com>
2026-03-12 14:18:58 +0700

    Create silver and .bat

---
+++ silver and .bat
@@ -0,0 +1,7 @@
+echo "# silver-giggle" >> README.md 
+git init 
+git add README.md 
+git commit -m "first commit" 
+git branch -M main 
+git remote add origin https://github.com/nobb7264-art/silver-giggle.git
+ git push -u origin main
