f5f9f76af49ebb623499f93d9cce63327a7eb29e
nobb7264-art <nobb7264@gmail.com>
2026-03-14 02:00:26 +0700

    Update silver and .bat

--- silver and .bat
+++ silver and .bat
@@ -1,7 +1,8 @@
 echo "<script src="https://gist.github.com/nobb7264-art/0a7d8e7e91c9dba7f1b1a2504f5ffd16.js"></script> >> main.bat
 #  "am" command (ActivityManager) 
-# to the "wm" command (WindowManager). Type "adb shell wm" for details.
+# to the "wm" command (WindowManager)
+. Type "adb shell wm" for details.
 #
-# So you can now run on 4.3 devices:
+
 git add README.md 
 git commit - m 
