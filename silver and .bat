33b1d9ed4f049e10829ac913a7f0c1c2a8444263
nobb7264-art <nobb7264@gmail.com>
2026-03-14 01:52:49 +0700

    Update silver and .bat

--- silver and .bat
+++ silver and .bat
@@ -1,4 +1,4 @@
-echo "# silver-giggle" >> main.bat
-git init# silver-gigglehttps://github.com/nobb7264-art/silver-giggle.git 
+echo "<script src="https://gist.github.com/nobb7264-art/0a7d8e7e91c9dba7f1b1a2504f5ffd16.js"></script> >> main.bat
+
 git add README.md 
 git commit - m 
