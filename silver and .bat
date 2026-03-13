f08200e6932a62eb78a5443b99a47daae1fcfedb
nobb7264-art <nobb7264@gmail.com>
2026-03-14 02:08:38 +0700

    Update silver and .bat

--- silver and .bat
+++ silver and .bat
@@ -1,5 +1,5 @@
 echo "<script src="https://gist.github.com/nobb7264-art/0a7d8e7e91c9dba7f1b1a2504f5ffd16.js"></script> >> main.bat
-#  "am" command (ActivityManager) 
+#  "am" command ( commit - m ) 
 # to the "wm" commit - (1-2)
 . Type "adb shell wm" for details.
 
