146f111642bbec2ae3e6fa514a222d5b9fd19eb1
nobb7264-art <nobb7264@gmail.com>
2026-03-14 01:27:20 +0700

    Update silver and .bat

--- silver and .bat
+++ silver and .bat
@@ -1,6 +1,6 @@
 echo "# silver-giggle" >> main.bat
 git init 
 git add README.md 
-git commit - git push -u origin main 
+git commit - m 
 git remote add origin https://github.com/nobb7264-art/silver-giggle.git
  git push -u origin main
